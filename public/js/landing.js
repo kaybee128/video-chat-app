ff4e97b3f939e54daca5791e4d225929faabc2dc
kritika <bansalk128@gmail.com>
2021-07-13 00:57:17 +0530

    second commit

---
+++ public/js/landing.js
@@ -0,0 +1,116 @@
+
+const createButton = document.querySelector("#create-a-room");
+const videoCont = document.querySelector('.video-my');
+const codeCont = document.querySelector('#joinroomcode');
+const joinBut = document.querySelector('#joinit');
+const mic = document.querySelector('#mic');
+const cam = document.querySelector('#webcam');
+
+let micAllowed = 1;
+let camAllowed = 1;
+
+let mediaConstraints = { video: true, audio: true };
+
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+    .then(localstream => {
+        videoCont.srcObject = localstream;
+    })
+
+function uuidv4() {
+    return 'xxyxyxxyx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const createroomtext = 'Creating Room...';
+
+createButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    createButton.disabled = true;
+    createButton.innerHTML = 'Creating Room';
+    createButton.classList = 'create-a-room-clicked';
+
+    setInterval(() => {
+        if (createButton.innerHTML < createroomtext) {
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length + 1);
+        }
+        else {
+            createButton.innerHTML = createroomtext.substring(0, createButton.innerHTML.length - 3);
+        }
+    }, 500);
+
+    //const name = nameArea.value;
+    location.href = `/room.html?room=${uuidv4()}`;
+});
+
+joinBut.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() == "") {
+        codeCont.classList.add('joinroomcode-error');
+        return;
+    }
+    const code = codeCont.value;
+    location.href = `/room.html?room=${code}`;
+})
+
+codeCont.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (codeCont.value.trim() !== "") {
+        codeCont.classList.remove('joinroomcode-error');
+        return;
+    }
+})
+
+cam.addEventListener('click', () => {
+    if (camAllowed) {
+        mediaConstraints = { video: false, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "nodevice";
+        cam.innerHTML = `<i class="fas fa-video-slash"></i>`;
+        camAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: true, audio: micAllowed ? true : false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        cam.classList = "device";
+        cam.innerHTML = `<i class="fas fa-video"></i>`;
+        camAllowed = 1;
+    }
+})
+
+mic.addEventListener('click', () => {
+    if (micAllowed) {
+        mediaConstraints = { video: camAllowed ? true : false, audio: false };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.classList = "nodevice";
+        mic.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+        micAllowed = 0;
+    }
+    else {
+        mediaConstraints = { video: camAllowed ? true : false, audio: true };
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then(localstream => {
+                videoCont.srcObject = localstream;
+            })
+
+        mic.innerHTML = `<i class="fas fa-microphone"></i>`;
+        mic.classList = "device";
+        micAllowed = 1;
+    }
+})
+
+
+
